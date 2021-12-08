2e8e00edb70daa0fb1476195e220d769fff9841b
Andreii <zsleshz@gmail.com>
2021-12-08 14:30:47 +0200

    newTask

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log(1);
+'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movie: {},
+  actors: {},
+  genres: [],
+  privat: false
+
+};
+
+const questionFirst = prompt ('Один из последних просмотренных фильмов?', '');
+const questionSecond = prompt ('На сколько оцените его?', '');
+console.log(movie);
