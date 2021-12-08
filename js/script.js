a6356948a0bf6017ca5ff2f67fa651f2d7f33c17
Andreii <zsleshz@gmail.com>
2021-12-08 21:31:49 +0200

    Script js file addition

--- js/script.js
+++ js/script.js
@@ -2,13 +2,18 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
   count: numberOfFilms,
-  movie: {},
+  movies: {},
   actors: {},
   genres: [],
   privat: false
 
 };
 
-const questionFirst = prompt ('Один из последних просмотренных фильмов?', '');
-const questionSecond = prompt ('На сколько оцените его?', '');
-console.log(movie);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
