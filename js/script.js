ee7c66d888c79009547cfcc27f69242296fac3e0
Andreii <zsleshz@gmail.com>
2021-12-10 23:48:55 +0200

    This issolution of the task

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
 'use strict';
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
 
-// };
+};
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = prompt('На сколько оцените его?', '');
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-// console.log(personalMovieDB);
-//////////////////////////////////////////////////////////////
-if ( 5 == 7) {
-    console.log('Верно');
-} else {
-    console.log('No Discription');
+console.log(personalMovieDB);
+////////////////////////////////////////////////////////////
+// if ( 5 == 7) {
+//     console.log('Верно');
+// } else {
+//     console.log('No Discription');
+// }
+//////////////////////////
+// if (personalMovieDB.count < 10) {
+//     console.log(alert("Просмотрено довольно мало фильмов"));
+// } 
+// else {
+//   console.log(alert("Вы классический зритель"));
+// }
+/////////////////////////////////////
+/////////////////////////////////////
+for (let i = 0; i < 2; i++){
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+        
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50){
+          personalMovieDB.movies[a] = b;
+          console.log('This Ok');
+        } else {
+            console.log('error');
+            i--;
+        }
 }
+
+console.log(personalMovieDB);
