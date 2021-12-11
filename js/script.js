47089ee4080543db91e2c90546ade02be712120c
Andreii <zsleshz@gmail.com>
2021-12-11 23:08:38 +0200

    new lesson while

--- js/script.js
+++ js/script.js
@@ -18,26 +18,58 @@ console.log(personalMovieDB);
 //     console.log('No Discription');
 // }
 //////////////////////////
-// if (personalMovieDB.count < 10) {
-//     console.log(alert("Просмотрено довольно мало фильмов"));
-// } 
-// else {
-//   console.log(alert("Вы классический зритель"));
-// }
 /////////////////////////////////////
 /////////////////////////////////////
-for (let i = 0; i < 2; i++){
-  const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
+// for (let i = 0; i < 3; i++){
+//   const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
         
 
-        if (a != null && b != null && a != '' && b != '' && a.length < 50){
-          personalMovieDB.movies[a] = b;
-          console.log('This Ok');
-        } else {
-            console.log('error');
-            i--;
-        }
-}
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//           personalMovieDB.movies[a] = b;
+//           console.log('This Ok');
+//         } else {
+//             console.log('error');
+//             i--;//если условие не выполнилось вернуться на одру итерацию назад.
+//         }
+// } if (personalMovieDB.count < 10) {
+//   console.log(alert("Просмотрено довольно мало фильмов"));
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+// console.log(alert("Вы классический зритель"));
+// } else if (personalMovieDB.count >= 30)  {
+//     console.log("Вы Киноман");
+//   } else {
+//     console.log("Произошла ошибка");
+//   }
 
+
+////////////////////////////////////////////////////////////////////////
+// let x = 0;
+// while (x < 1){
+//     const   a = prompt('Последний фильм?', ''),
+//             b = +prompt('Ваша оценка?', '');
+//     if(a != null && b != null && a != '' && b != '' && a.length < 50){
+//     // Если а не "отмена" и b не "отмена" и a не пустая строка и b не пустая строка и а < 50 символов то
+//     personalMovieDB.movies[a] = b;
+//     console.log("Done!");
+//     } else {
+//         console.log("Error");
+//         x--;
+//     }
+//     x++;
+// }
+// Second
+let x = 0;
+while (x < 1){
+    const   a = prompt('Последний фильм?', ''),
+            b = +prompt('Ваша оценка?', '');
+    (a != null && b != null && a != '' && b != '' && a.length < 50) ? personalMovieDB.movies[a] = b : x--, console.log("Error");
+    x++;
+}
 console.log(personalMovieDB);
+//////////////////////////////////////////////
+///Function
+///////////////////
+function showFirstMessage( {
+  
+})
